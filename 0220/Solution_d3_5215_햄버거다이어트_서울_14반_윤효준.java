9d47dc70c4972f69395e1453700a860024ab54f0
yoonc01 <yoonc01@hotmail.com>
2025-02-21 16:06:05 +0900

    feat: Fix 0220 햄버거 다이어트를 조합을 사용하는 방식으로 수정

--- 0220/Solution_d3_5215_햄버거다이어트_서울_14반_윤효준.java
+++ 0220/Solution_d3_5215_햄버거다이어트_서울_14반_윤효준.java
@@ -1,46 +1,45 @@
 import java.io.*;
 import java.util.*;
 
+// 조합으로 풀이
 public class Solution_d3_5215_햄버거다이어트_서울_14반_윤효준 {
+    static int n, r, limit, ans;
+    static int[][] data;
+
+    static void comb(int cnt, int start, int totalScore, int totalCalorie) {
+        if (cnt == r)
+        {
+            ans = Math.max(ans, totalScore);
+            return;
+        }
+        for (int i = start; i < n; i++)
+        {
+            if (totalCalorie + data[i][1] <= limit)
+                comb(cnt + 1, i + 1, totalScore + data[i][0], totalCalorie + data[i][1]);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder resultBuilder = new StringBuilder();
 
-        int T = Integer.parseInt(reader.readLine());
+        int T = Integer.parseInt(br.readLine());
 
         for (int testCase = 1; testCase <= T; testCase++) {
-            StringTokenizer st = new StringTokenizer(reader.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int limit = Integer.parseInt(st.nextToken());
-
-            int[][] dp = new int[n + 1][limit + 1];
-            int[][] data = new int[n][2];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            limit = Integer.parseInt(st.nextToken());
+            data = new int[n][2];
+            ans = 0;
 
             for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(reader.readLine());
-                int score = Integer.parseInt(st.nextToken());
-                int calorie = Integer.parseInt(st.nextToken());
-                data[i][0] = score;
-                data[i][1] = calorie;
+                st = new StringTokenizer(br.readLine());
+                data[i][0] = Integer.parseInt(st.nextToken());
+                data[i][1] = Integer.parseInt(st.nextToken());
             }
-
-            for (int j = 0; j <= limit; j++) {
-                dp[0][j] = 0;
-            }
-
-            int ans = 0;
-
-            for (int i = 1; i <= n; i++) {
-                int score = data[i - 1][0];
-                int calorie = data[i - 1][1];
-                for (int j = 0; j <= limit; j++) {
-                    dp[i][j] = dp[i - 1][j];
-                    if (j >= calorie) {
-                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - calorie] + score);
-                    }
-                    ans = Math.max(ans, dp[i][j]);
-                }
-            }
+            // nC0, nC1 ... nCn
+            for (r = 0; r <= n; r++)
+                comb(0, 0, 0, 0);
 
             resultBuilder.append("#").append(testCase).append(" ").append(ans).append("\n");
         }
